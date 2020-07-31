e4456be0f60df5dccf7b00345d52f261a6a82893
Philip DeFraties <philipjfry338@gmail.coom>
2020-07-31 13:12:07 -0600

    Add read method.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,11 @@
 class Translator
-  
+
+  def initialize(eng_text_file_path)
+    @eng_text = eng_text_file_path
+  end
+
+  def read_from_english_file
+    p File.read(@eng_text) 
+  end
+
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -5,9 +5,16 @@ require './lib/translator.rb'
 class TranslatorTest < MiniTest::Test
 
   def test_it_exists
-    translator = Translator.new
+    translator = Translator.new('message.txt')
 
     assert_instance_of Translator, translator
+  end
+
+
+  def test_it_can_print_from_eng_file
+    translator = Translator.new('message.txt')
+
+    assert_equal "hello world\n", translator.read_from_english_file
   end
 
 end
